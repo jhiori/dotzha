98b0b2c0731506655931007052381bed0202a56d
Jhiori <worldhacker@hotmail.es>
2023-08-04 18:44:34 -0600

    Using AppRoutingModule

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,18 +1,18 @@
-import { RouterModule } from "@angular/router";
-import { AppComponent } from "./app.component";
-import { DefaulterComponent } from "./components/defaulter/defaulter.component";
-import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
-import { BuildingPageComponent } from "./components/building-page/building-page.component";
+// import { RouterModule } from "@angular/router";
+// import { AppComponent } from "./app.component";
+// import { DefaulterComponent } from "./components/defaulter/defaulter.component";
+// import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+// import { BuildingPageComponent } from "./components/building-page/building-page.component";
 
-type PathMatch = "full" | "prefix" | undefined
+// type PathMatch = "full" | "prefix" | undefined
 
-const pathmatch: PathMatch = 'full'
+// const pathmatch: PathMatch = 'full'
 
-const appRoutes = [
-    { path: '', component: BuildingPageComponent},
-    { path: 'defaulter', component: DefaulterComponent, pathMatch: pathmatch},
-    // { path: '', redirectTo: "/defaulter", pathMatch: pathmatch},
-    { path: '**', component: PageNotFoundComponent}
-];
+// const appRoutes = [
+//     { path: '', component: BuildingPageComponent},
+//     { path: 'defaulter', component: DefaulterComponent, pathMatch: pathmatch},
+//     // { path: '', redirectTo: "/defaulter", pathMatch: pathmatch},
+//     { path: '**', component: PageNotFoundComponent}
+// ];
 
-export const routing = RouterModule.forRoot(appRoutes);
+// export const routing = RouterModule.forRoot(appRoutes);
