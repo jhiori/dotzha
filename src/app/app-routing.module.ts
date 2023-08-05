36446f794da13377e81506a57e862c201efd42af
Jhiori <worldhacker@hotmail.es>
2023-08-04 19:38:43 -0600

    Chage on path routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,11 @@ import { DefaulterComponent } from "./components/defaulter/defaulter.component";
 import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 import { BuildingPageComponent } from "./components/building-page/building-page.component";
 
-type PathMatch = "full" | "prefix" | undefined
-const pathmatch: PathMatch = 'full'
+// type PathMatch = "full" | "prefix" | undefined
+// const pathmatch: PathMatch = 'full'
 const routes: Routes = [
   { path: '', component: BuildingPageComponent},
-  { path: 'defaulter', component: DefaulterComponent, pathMatch: pathmatch},
+  { path: 'defaulter', component: DefaulterComponent},
   // { path: '', redirectTo: "/defaulter", pathMatch: pathmatch},
   { path: '**', component: PageNotFoundComponent}
 ];
