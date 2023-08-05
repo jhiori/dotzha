15f6ad0a00f6eb1a58877be02d73eff20071ecbb
Jhiori <worldhacker@hotmail.es>
2023-08-04 22:54:37 -0600

    Deploy adding 404 file and test adding path on constructor

---
+++ docs/app/models/compay.model.ts
@@ -0,0 +1,8 @@
+export interface Company{
+  id: number;
+  nameCompany: string;
+  descriptionCompany: string;
+  imgCompany: string;
+  pinCompany: string; //Ubicacion de la empresa
+  contacUsCompany?: string;
+}
