6d011f121bacdd7430b3ee7d00280059f96952b1
jhiori <worldhacker@hotmail.es>
2025-07-25 19:56:53 -0600

    project update

--- src/app/components/building-page/building-page.component.ts
+++ src/app/components/building-page/building-page.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-building-page',
+  
   templateUrl: './building-page.component.html',
   styleUrls: ['./building-page.component.scss']
 })
