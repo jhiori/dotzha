4a8c757d168580b2f54955ece40fbc1875eae7ae
jhiori <worldhacker@hotmail.es>
2025-07-30 13:31:44 -0600

    Update material button

--- src/app/components/building-page/building-page.component.ts
+++ src/app/components/building-page/building-page.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-building-page',
   standalone:true,
   imports: [
     MatProgressBarModule,
-    MatIconModule
-
+    MatIconModule,
+    MatButtonModule
+    
   ],
   templateUrl: './building-page.component.html',
   styleUrl: './building-page.component.scss'
