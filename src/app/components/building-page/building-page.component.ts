b86dbdd25fa202b65c89fe74e08b05fcc65257f4
jhiori <worldhacker@hotmail.es>
2025-07-30 20:05:00 -0600

    svg on local project

--- src/app/components/building-page/building-page.component.ts
+++ src/app/components/building-page/building-page.component.ts
@@ -1,19 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatIconModule} from '@angular/material/icon';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { DomSanitizer } from '@angular/platform-browser';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-building-page',
   standalone:true,
   imports: [
+    
     MatProgressBarModule,
-    MatIconModule,
+    MatIconModule,// MatIconRegistry,    
     MatButtonModule
     
   ],
+  // providers: [
+  //   provideHttpClient(),
+  //   // ... other providers like provideAnimationsAsync()
+  // ],
   templateUrl: './building-page.component.html',
   styleUrl: './building-page.component.scss'
 })
-export class BuildingPageComponent {
 
+// export const appConfig: BuildingPageComponent = {
+//   providers: [
+//     // ... other providers you might have
+//     provideHttpClient(), // Add this line
+//   ],
+// };
+export class BuildingPageComponent {
+  
+  constructor( private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer ) {  
+    this.matIconRegistry.addSvgIcon('dotzha-name',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('/building-maintenance/dotzha_Name.svg')); 
+  }
 }
